08b4dabc9e77a341eab4bd5caa7a2c72d2e540f2
Prince clumson-eklu <ameto888@protonmail.com>
2025-01-08 13:45:57 -0500

    Add a Provider class to Provider

---
+++ time_tracker/lib/provider/project_task_provider.dart
@@ -0,0 +1,16 @@
+// Step 3: Add a provider
+class TimeEntryProvider with ChangeNotifier {
+  List<TimeEntry> _entries = [];
+
+  List<TimeEntry> get entries => _entries;
+
+  void addTimeEntry(TimeEntry entry) {
+    _entries.add(entry);
+    notifyListeners();
+  }
+
+  void deleteTimeEntry(String id) {
+    _entries.removeWhere((entry) => entry.id == id);
+    notifyListeners();
+  }
+}
