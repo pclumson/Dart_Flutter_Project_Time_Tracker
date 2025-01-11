c8b45f028be710fdbb0f2cc211f7c96d01c7bcc4
Prince clumson-eklu <ameto888@protonmail.com>
2025-01-11 09:21:41 -0500

    Add from and to json conversion to Task model

--- time_tracker/lib/models/task.dart
+++ time_tracker/lib/models/task.dart
@@ -3,5 +3,29 @@ class Task {
   final String id;
   final String name;
 
-  Task({required this.id, required this.name});
+  Task({
+    required this.id,
+    required this.name
+
+  });
+
+  // Convert a JSON object to an Task instance
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+
+      id: json['id'],
+      name: json['name'],
+
+    );
+  }
+
+  // Convert a Task instance to a JSON object
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+
+    };
+
+
 }
