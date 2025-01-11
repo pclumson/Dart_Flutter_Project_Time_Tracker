d6c34ca868f5226e36f76793282714c2d93e283b
Prince clumson-eklu <ameto888@protonmail.com>
2025-01-11 11:25:06 -0500

    Update provider class to load and save to local storage

--- time_tracker/lib/providers/project_task_provider.dart
+++ time_tracker/lib/providers/project_task_provider.dart
@@ -1,16 +1,86 @@
 // Step 3: Add a provider
+import 'package:flutter/foundation.dart';
+import '../models/project.dart';
+import '../models/task.dart';
+import '../models/time_entry.dart';
+import 'package:localstorage/localstorage.dart';
+import 'dart:convert';
+
+
+
+
 class TimeEntryProvider with ChangeNotifier {
+
+
+  final LocalStorage storage;
+
+  // List of TimeEntry
   List<TimeEntry> _entries = [];
+  // List of Task
+//   List<Task> _entries = [];
+//   // List of Project
+//   List<Project> _entries = [];
+//
 
+  // Getters
   List<TimeEntry> get entries => _entries;
+//   List<Task> get entries => _entries;
+//   List<Project> get entries => _entries;
+//
 
+//   List<TimeEntry> get time_entries => _time_entries;
+//   List<Task> get tasks => _tasks;
+//   List<Project> get projects => _projects;
+//
+
+
+  TimeEntryProvider(this.storage) {
+    _loadTimeEntryFromStorage();
+  }
+
+  void _loadTimeEntryFromStorage() async {
+    // await storage.ready;
+    var storedTimeEntry = storage.getItem('entries');
+    if (storedTimeEntry != null) {
+      _entries = List<TimeEntries>.from(
+        (storedTimeEntry as List).map((item) => TimeEntry.fromJson(item)),
+      );
+      notifyListeners();
+    }
+  }
+
+// Add Time entry to storage
   void addTimeEntry(TimeEntry entry) {
     _entries.add(entry);
+    _saveTimeEntryToStorage();
     notifyListeners();
   }
 
+  // Save timeEntry to storage
+  void _saveTimeEntryToStorage() {
+    storage.setItem(
+      'entries', jsonEncode(_entries.map((e) => e.toJson()).toList()));
+  }
+
+  // Add or Update time entry
+  void addOrUpdateTimeEntry(TimeEntry entry) {
+    int index = _entries.indexWhere((e) => e.id == entry.id);
+    if (index != -1) {
+      // Update existing entry
+      _entries[index] = entry;
+    } else {
+      // Add new entry
+      _entries.add(entry);
+    }
+    _saveTimeEntryToStorage(); // Save the updated list to local storage
+    notifyListeners();
+  }
+
+
+  // Delete timeEntry
   void deleteTimeEntry(String id) {
     _entries.removeWhere((entry) => entry.id == id);
+    _saveTimeEntryToStorage();
     notifyListeners();
   }
 }
