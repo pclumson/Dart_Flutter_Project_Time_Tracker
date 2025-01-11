68390d21494c8d99e54a6abfe29b9683cceb66df
Prince clumson-eklu <ameto888@protonmail.com>
2025-01-11 09:18:37 -0500

    Add from and to json conversion to Project model

--- time_tracker/lib/models/project.dart
+++ time_tracker/lib/models/project.dart
@@ -2,5 +2,30 @@ class Project {
   final String id;
   final String name;
 
-  Project({required this.id, required this.name});
+  Project({
+
+    required this.id,
+    required this.name
+
+  });
+
+  // Convert a JSON object to an Project instance
+  factory Project.fromJson(Map<String, dynamic> json) {
+    return Project(
+
+      id: json['id'],
+      name: json['name'],
+
+    );
+  }
+
+  // Convert a Project instance to a JSON object
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+
+      'name': name,
+
+    };
+
 }
